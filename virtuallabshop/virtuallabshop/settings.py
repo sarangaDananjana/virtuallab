824ed2568f66f3f71afb246a1bc3d1bedd3ccd9d
sarangaDananjana <dananjanasaranga@gmail.com>
2025-08-14 22:17:20 +0530

    updated settings.py file

--- virtuallabshop/virtuallabshop/settings.py
+++ virtuallabshop/virtuallabshop/settings.py
@@ -20,15 +20,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-okb1mhljv--^gb_tpu0qg&yqw$uc__3-fjuy^m4p$)jpui(a_h'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["www.virtuallabgames.com", "virtuallabgames.com"]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+
+
+SECURE_HSTS_SECONDS = 60               # try 60s first
+# set True only if ALL subdomains use HTTPS
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_PRELOAD = False
+
 
 # Application definition
 
