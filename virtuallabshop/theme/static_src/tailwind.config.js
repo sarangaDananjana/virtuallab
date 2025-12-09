8b69c0b50eaab3ec0a901e4780ba35ea3c7e6d3f
sarangaDananjana <dananjanasaranga@gmail.com>
2025-12-09 16:11:29 +0530

    s

--- virtuallabshop/theme/static_src/tailwind.config.js
+++ virtuallabshop/theme/static_src/tailwind.config.js
@@ -27,10 +27,5 @@ module.exports = {
             }
             // -------------------------------------
         },
-    },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/aspect-ratio'),
-    ],
+    }
 }
